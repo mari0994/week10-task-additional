ca6db86459b03e818e6f4d64902bbd54f7f85bb2
mari0994 <mbabiy@mail.ru>
2023-06-16 22:17:45 +0300

    edit script

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-function calculateTotalPrice(quantity=2, price=3000){
+/*function calculateTotalPrice(quantity=2, price=3000){
     const totalPrice = quantity * price;
     alert(`Стоимость вашей покупки - ${totalPrice.toLocaleString('ru-RU', { style: 'currency', currency: 'RUB' })}`);
+}*/ 
+//вариант с alert
+
+function calculateTotalPrice(quantity=2, price=3000){
+    const totalPrice = quantity * price;
+    const total = document.getElementById('total');
+    let message = `Стоимость вашей покупки - ${totalPrice.toLocaleString('ru-RU', { style: 'currency', currency: 'RUB' })}`;
+    total.textContent = (message);
 }
